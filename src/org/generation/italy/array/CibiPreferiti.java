6aa0ae60c7370dc93ee8d5934494c064181f6e2d
FabioCividini <fabiocividini22@gmail.com>
2021-11-17 14:52:20 +0100

    inizializzato array e stampato lunghezza classifica a video

---
+++ src/org/generation/italy/array/CibiPreferiti.java
@@ -0,0 +1,15 @@
+package org.generation.italy.array;
+
+public class CibiPreferiti {
+
+	public static void main(String[] args) {
+		//inizializzazione array
+		String[] classificaCibi = {"Pizza" ,"Cozza gratinate" ,"Carbonara" ,"Astice" ,"Grigliata di carne" ,"Sushi" ,"Grigliata di pesce" ,"Formaggio e miele" ,"Risotto pere taleggio"};
+		
+		//lunghezza della classifica
+		System.out.println("Numero totale classifica dei cibi preferiti: " + classificaCibi.length);
+		
+		
+	}
+
+}
